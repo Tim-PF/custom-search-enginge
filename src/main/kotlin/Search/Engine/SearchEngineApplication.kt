58ae542f2fac7c8d1dc830586c5d8980be9fa4eb
timpf <tim.pfops@gmail.com>
2026-03-21 19:01:19 +0100

    initialize spring boot project

---
+++ src/main/kotlin/Search/Engine/SearchEngineApplication.kt
@@ -0,0 +1,11 @@
+package Search.Engine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SearchEngineApplication>(*args)
+}
---
+++ src/test/kotlin/Search/Engine/SearchEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package Search.Engine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchEngineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
