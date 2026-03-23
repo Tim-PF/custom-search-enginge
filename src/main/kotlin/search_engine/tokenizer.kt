dd94d87a6418736b6f91de17bbefa360f46982bd
timpf <tim.pfops@gmail.com>
2026-03-23 13:35:10 +0100

    Improve tokenization by addding lowercase,regex to remove unwanted chars and remove empty tokens

--- src/main/kotlin/search_engine/tokenizer.kt
+++ src/main/kotlin/search_engine/tokenizer.kt
@@ -1,6 +1,6 @@
 package search_engine
 
 fun tokenize(text: String): List<String> {
-    val wordsList = text.split(" ")
+    val wordsList = text.lowercase().split(Regex("[^a-zA-Z0-9]+")).filter { it.isNotEmpty() }
     return wordsList
 }
