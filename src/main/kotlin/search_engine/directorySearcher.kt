6859b68a776fcf79fa1a0a86480188e35c74c193
timpf <tim.pfops@gmail.com>
2026-03-25 13:52:58 +0100

    Index token to data files

--- src/main/kotlin/search_engine/directorySearcher.kt
+++ src/main/kotlin/search_engine/directorySearcher.kt
@@ -9,8 +9,17 @@ fun readfile(file: File) : String {
 fun listOfFiles() {
     val files = File("data").listFiles() ?: return
     val fileList = mapFiles(files)
+    val index = mutableMapOf<String,List<Int>>()
     fileList.forEach {
         println(it.value.name)
         val tokenList = tokenize(readfile(it.value))
+        for (word in tokenList) {
+            if (index.containsKey(word)) {
+                index[word] = index[word]!!.plus(it.key)
+            } else {
+                index[word] = listOf(it.key)
+            }
+        }
     }
+    println(index);
 }
